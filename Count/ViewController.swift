577e793ba200c7f8e1575ad3bff12da68c855c90
shocho0101 <shocho0101@hotmail.co.jp>
2021-05-09 17:08:08 +0900

    Initial Commit

---
+++ Count/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  Count
+//
+//  Created by 張翔 on 2021/05/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var label: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // 画面が初めて表示された時に実行
+        
+        
+    }
+    
+    @IBAction func aaaaaaaaaaaaa() {
+        
+        
+        
+    }
+    
+    @IBAction func bbbbbbbbbbbb() {
+        
+        
+        
+    }
+    
+
+    
+
+
+}
+
